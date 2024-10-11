6b098538019faf56dd76e820b4fdedd9fbb7ce21
Dmitry Tsurko <dmytro.tsurko@virtuozzo.com>
2024-10-11 11:42:12 +0300

    JE-67892 Create addon for Node.js installation

--- scripts/check_distro.sh
+++ scripts/check_distro.sh
@@ -4,4 +4,7 @@ glibcMinVersion=2.28;
 glibcVersion=$(rpm -q glibc | awk -F '-' '{print $2}'); 
 if awk "BEGIN {exit !(${glibcVersion} < ${glibcMinVersion})}"; then
     echo "Non-supported"; exit 0; 
-fi    
+fi
+if grep -q AlmaLinux /etc/os-release; then
+    echo "AlmaLinux"
+fi
